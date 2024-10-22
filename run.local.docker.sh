c9f7db59033dec886b03a22cc1769e98ac881e36
Asias He <asias@scylladb.com>
2024-10-22 21:02:48 +0800

    run.local.docker.sh

---
+++ run.local.docker.sh
@@ -0,0 +1,5 @@
+#scylla-6.1.2
+export SCT_SCYLLA_VERSION=6.1.2
+export SCT_ENABLE_ARGUS=False
+#proxychains4 hydra run-test longevity_test.LongevityTest.test_custom_time --backend docker --config test-cases/PR-provision-test-docker.yaml
+proxychains4 hydra run-test performance_regression_test.PerformanceRegressionTest.test_latency_mixed_with_nemesis --backend docker --config test-cases/performance/perf-regression-latency-650gb-with-nemesis.yaml  --config configurations/tablets_disabled.yaml --config configurations/disable_kms.yaml
