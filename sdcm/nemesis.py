3db9caf833a4fc803bf5e9ab8d5825887f188515
Asias He <asias@scylladb.com>
2024-10-22 20:07:58 +0800

    ci

--- sdcm/nemesis.py
+++ sdcm/nemesis.py
@@ -1675,23 +1675,20 @@ class Nemesis:  # pylint: disable=too-many-instance-attributes,too-many-public-m
 
         end_time = time.time()
         time_elapsed = int(end_time - start_time)
-        LOGGER.info(f"HJ: Finished repair on db nodes time_elapsed={time_elapsed}")
-        time.sleep(60)
+        LOGGER.info(f"HJ: Finished repair on db nodes time_elapsed={time_elapsed}s")
 
     @latency_calculator_decorator(legend="Run repair on all nodes")
     def disrupt_no_corrupt_repair_all_nodes_old(self):
-        self.log.debug('Prepare test table if they do not exist')
         self._prepare_test_table(ks=f'keyspace1', table='standard1')
         self.cluster.wait_for_schema_agreement()
 
         cmd = "ALTER TABLE keyspace1.standard1 WITH tombstone_gc = {'mode': 'repair'};"
-        LOGGER.info(f"Set gc mode to repair: {cmd}")
+        LOGGER.info(f"HJ: Set gc mode to repair: {cmd}")
         self.target_node.run_cqlsh(cmd)
 
         def _nodetool_repair(node):
-            LOGGER.info(f"Run nodetool repair on {node}")
-            #with adaptive_timeout(Operations.REPAIR, node, timeout=HOUR_IN_SEC * 48):
-            node.run_nodetool(sub_cmd="repair -pr keyspace1", long_running=True, retry=0)
+            LOGGER.info(f"HJ: Run nodetool repair on {node}")
+            node.run_nodetool(sub_cmd="repair -pr keyspace1", long_running=False, retry=0)
 
         start_time = time.time()
         LOGGER.info("HJ: Started repair on db nodes")
@@ -1702,8 +1699,7 @@ class Nemesis:  # pylint: disable=too-many-instance-attributes,too-many-public-m
 
         end_time = time.time()
         time_elapsed = int(end_time - start_time)
-        LOGGER.info(f"HJ: Finished repair on db nodes time_elapsed={time_elapsed}")
-        time.sleep(60)
+        LOGGER.info(f"HJ: Finished repair on db nodes time_elapsed={time_elapsed}s")
 
     def _major_compaction(self):
         with adaptive_timeout(Operations.MAJOR_COMPACT, self.target_node, timeout=8000):
@@ -5615,7 +5611,8 @@ class NoCorruptRepairAllNodesMonkey(Nemesis):
     limited = True
 
     def disrupt(self):
-        self.disrupt_no_corrupt_repair_all_nodes()
+        #self.disrupt_no_corrupt_repair_all_nodes()
+        self.disrupt_no_corrupt_repair_all_nodes_old()
 
 
 class MajorCompactionMonkey(Nemesis):
