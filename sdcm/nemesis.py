874cb33a100c47ee69e5168a1ceebb2f0e8c026c
Asias He <asias@scylladb.com>
2024-10-24 16:51:44 +0800

    1M

--- sdcm/nemesis.py
+++ sdcm/nemesis.py
@@ -2125,7 +2125,8 @@ class Nemesis:  # pylint: disable=too-many-instance-attributes,too-many-public-m
             cs_thread.verify_results()
 
     def _prepare_test_table2(self, ks='keyspace1', table=None):
-        stress_cmd = "cassandra-stress write n=1000 cl=QUORUM -mode native cql3 " \
+        #stress_cmd = "cassandra-stress write n=1000 cl=QUORUM -mode native cql3 " \
+        stress_cmd = "cassandra-stress write n=1000000 cl=QUORUM -mode native cql3 " \
                      f"-schema 'replication(strategy=NetworkTopologyStrategy," \
                      f"replication_factor={self.tester.reliable_replication_factor})' -log interval=5"
         cs_thread = self.tester.run_stress_thread(
