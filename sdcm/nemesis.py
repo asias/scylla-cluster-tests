e2ac05325222983e75f71b4343ab229521c456ca
Asias He <asias@scylladb.com>
2024-11-21 17:01:58 +0800

    400M sleep 15m

--- sdcm/nemesis.py
+++ sdcm/nemesis.py
@@ -1838,6 +1838,8 @@ class Nemesis:  # pylint: disable=too-many-instance-attributes,too-many-public-m
         table = 'standard1'
         # 100M
         key_nr = 100000000
+        # 400M
+        key_nr = 400000000
         start_key = 1
         end_key = key_nr
         column_size = 34
@@ -1859,7 +1861,12 @@ class Nemesis:  # pylint: disable=too-many-instance-attributes,too-many-public-m
         insert_time = int(time.time() - start_time)
         LOGGER.info(f"HJ: Finished insert {keyspaces=} nodes={nr_nodes} {key_nr=} {insert_time=}s")
 
+        sleep_before_repair = 900
         for ks in keyspaces:
+            LOGGER.info(f"HJ: Started to sleep before repair {ks=}")
+            time.sleep(sleep_before_repair)
+            LOGGER.info(f"HJ: Finished to sleep before repair {ks=}")
+
             # Modify config
             node = nodes[0]
             enable_multiple_dc_opt = False
@@ -1877,9 +1884,9 @@ class Nemesis:  # pylint: disable=too-many-instance-attributes,too-many-public-m
             repair_time = int(time.time() - start_time)
             LOGGER.info(f"HJ: Finished repair {ks=} {nr_nodes=} {key_nr=} {nr_dc=} {rf_per_dc=} {repair_time=}s {enable_multiple_dc_opt=}")
 
-        LOGGER.info(f"HJ: Started to sleep")
+        LOGGER.info(f"HJ: Started to sleep after test")
         time.sleep(600)
-        LOGGER.info(f"HJ: Finished to sleep")
+        LOGGER.info(f"HJ: Finished to sleep after test")
 
     @latency_calculator_decorator(legend="Run insert while node down and repair")
     def disrupt_insert_with_node_down_repair(self, keyspaces=["ks1", "ks2"]):
